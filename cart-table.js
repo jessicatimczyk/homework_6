fce993d3eb11719037c8653400008ac4f9408d2f
jessicatimczyk <jtimczyk@andrew.cmu.edu>
2020-11-08 01:12:31 -0500

    cart number update

--- cart-table.js
+++ cart-table.js
@@ -4,7 +4,6 @@ console.log("cart products = ", sessionStorage.cart_products);
 
 // Get array of classes without jQuery
 var cart_prod = JSON.parse(sessionStorage.cart_products);
-var remove_buttons = new Array();
 
 //var arrayLength = array.length;
 var theTable = document.createElement('table');
@@ -36,30 +35,13 @@ function createCart() {
         tr.appendChild(td2);
         theTable.appendChild(tr);
 
-        remove_buttons.push(butn);
-
-        //    butn.onclick = remove_prod();
-
-        //    butn.onclick = function () {
-        //        //        alert(butn.id);
-        //        console.log("cart_prod = ", cart_prod);
-        //        //        delete cart_prod[i];
-        //        cart_prod.splice(i, 1);
-        //        console.log("cart_prod after delete = ", cart_prod);
-        //    };
     }
     let my_table = document.getElementById("my_table");
     my_table.appendChild(theTable);
 
 }
-//for (var i = 0; i < remove_buttons.length; i++) {
-//    current_butn = document.getElementById(i);
-//    current_butn.
-//
-//}
 
 
-console.log('button array = ', remove_buttons);
 
 function remove_prod(name) {
     console.log("im in remove product!");
@@ -71,25 +53,16 @@ function remove_prod(name) {
 
     sessionStorage.cart_products = JSON.stringify(cart_prod);
     console.log('session storage = ', sessionStorage.cart_products);
-    var removed_product = document.getElementById("cart_table");
-    //    removed_product.parentNode.removeChild(removed_product);
     location.reload();
-    //    removed_product.remove();
-    //
-    //    createCart();
+
+    decreaseCartNum();
 
 }
 
+function decreaseCartNum() {
+    if (sessionStorage.items_in_cart) {
+        sessionStorage.items_in_cart = Number(sessionStorage.items_in_cart) - 1;
+    }
+}
 
-//function printButn() {
-//    for (var i = 0; i < cart_prod.length; i++) {
-//        var butn = document.createElement("button");
-//        var txt = document.createTextNode("Remove");
-//        butn.appendChild(txt);
-//        document.appendChild(butn);
-//        butn.classList.add("remove_btn");
-//    }
-//}
-
-//remove_prod();
 createCart();
